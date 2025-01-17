dd62d797ed577a8ed0a5965ddadc18b72a6b1f94
Alena1409 <a77993519@gmail.com>
2025-01-17 17:36:20 +0400

    fix: change code for 04-copy-directory, add removing files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,23 +6,39 @@ const filesCopyPath = path.join(__dirname, 'files-copy');
 
 fs.mkdir(filesCopyPath, { recursive: true }, (err) => {
   if (err) {
-    console.log('ошибка');
+    console.log('ошибкасоздания папки');
   }
-});
 
-fs.readdir(filesPath, { withFileTypes: true }, (err, files) => {
-  if (err) {
-    console.log(err);
-    return;
-  } else {
-    files.forEach((item) => {
-      const oldFile = path.join(filesPath, item.name);
-      const newFile = path.join(filesCopyPath, item.name);
-      fs.copyFile(oldFile, newFile, (err) => {
-        if (err) {
-          console.log('error');
-        }
+  fs.readdir(filesCopyPath, (err, copyFiles) => {
+    if (err) {
+      console.error('ошибка:', err);
+      return;
+    } else {
+      copyFiles.forEach((file) => {
+        const filePath = path.join(filesCopyPath, file);
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            console.error(`Ошибка удаления файла ${file}:`, err);
+          }
+        });
       });
+    }
+
+    fs.readdir(filesPath, { withFileTypes: true }, (err, files) => {
+      if (err) {
+        console.log(err);
+        return;
+      } else {
+        files.forEach((item) => {
+          const oldFile = path.join(filesPath, item.name);
+          const newFile = path.join(filesCopyPath, item.name);
+          fs.copyFile(oldFile, newFile, (err) => {
+            if (err) {
+              console.log('ошибка копирования');
+            }
+          });
+        });
+      }
     });
-  }
+  });
 });
