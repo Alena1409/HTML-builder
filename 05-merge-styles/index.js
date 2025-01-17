17db894b3bc76acf79e053dc5112b0f15f53075d
Alena1409 <a77993519@gmail.com>
2025-01-17 19:09:02 +0400

    fix: change code for 05-merge-styles and add renoving bundle.css

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,12 +3,31 @@ const path = require('path');
 
 const stylesPath = path.join(__dirname, 'styles');
 const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
+const folderPath = path.join(__dirname, 'project-dist');
 const allStyles = [];
 
 fs.readdir(stylesPath, { withFileTypes: true }, (err, files) => {
   if (err) {
     console.log('erroe: ', err);
   } else {
+    if (bundlePath) {
+      fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+        if (err) {
+          console.log('erroe: ', err);
+        } else {
+          files.forEach((files) => {
+            if (files.name === 'bundle.css') {
+              const filePath = path.join(folderPath, files.name);
+              fs.unlink(filePath, (err) => {
+                if (err) {
+                  console.error('error :', err);
+                }
+              });
+            }
+          });
+        }
+      });
+    }
     files.forEach((item) => {
       if (path.extname(item.name) === '.css') {
         const itemPath = path.join(stylesPath, item.name);
@@ -18,7 +37,7 @@ fs.readdir(stylesPath, { withFileTypes: true }, (err, files) => {
           } else {
             allStyles.push(data);
 
-            fs.appendFile(bundlePath, allStyles.join(''), (err) => {
+            fs.writeFile(bundlePath, allStyles.join(''), (err) => {
               if (err) {
                 console.log('error: ', err);
               }
