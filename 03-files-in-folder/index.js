d8b709766b6283d81921cc1c44a6fbe74256cf18
Alena1409 <a77993519@gmail.com>
2025-01-19 14:52:06 +0400

    fix: change code in 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,27 +7,19 @@ fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
   if (err) {
     console.log(err);
     return;
-  } else {
-    const result = [];
-    let len = 0;
-    files.forEach((item) => {
-      const filePath = path.join(folderPath, item.name);
-      const indexComa = item.name.lastIndexOf('.');
-      const name = item.name.slice(0, indexComa);
-      //const lastName = item.name.slice(indexComa + 1);
-      const lastName =
-        path.extname(item.name).slice(1) || 'тип файла не определен';
+  }
+  files.forEach((item) => {
+    const filePath = path.join(folderPath, item.name);
+    if (item.isFile()) {
+      const type = path.extname(filePath).slice(1);
+      const name = path.basename(filePath, `.${type}`);
       fs.stat(filePath, (err, stats) => {
         if (err) {
           console.error('Error:', err);
           return;
         }
-        result.push(`${name} - ${lastName} - ${stats.size} bt`);
-        len += 1;
-        if (len === files.length) {
-          console.log(result.join('\n'));
-        }
+        console.log(`${name} - ${type || 'unknown'} - ${stats.size} bytes`);
       });
-    });
-  }
+    }
+  });
 });
